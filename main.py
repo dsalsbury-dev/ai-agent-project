56ecda17698ddf5954bf7f62f7a7db5297b307d5
Dominic Salsbury <dominicsalsbury@proton.me>
2026-02-23 20:25:12 +0000

    Added argparser and implemented it so that it expects a single positional argument of type string as the user prompt when running main.py

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError(
-            "No GEMINI_API_KEY was found. Ensure one is available in envionrment variables")
+            "No GEMINI_API_KEY was found.")
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=args.user_prompt,
     )
+    if not response.usage_metadata:
+        raise RuntimeError(
+            "No usage_metadata. Likely the API request has failed.")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
