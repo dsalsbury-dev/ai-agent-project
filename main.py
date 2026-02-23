d115cac63afc2db64e522f9cd07bef9f0ed277a4
Dominic Salsbury <dominicsalsbury@proton.me>
2026-02-23 18:46:00 +0000

    Basic concept of how to perform requests to googles Gemini 2.5 flash model using Python

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from ai-agent-project!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError(
+            "No GEMINI_API_KEY was found. Ensure one is available in envionrment variables")
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
