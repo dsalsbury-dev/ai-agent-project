b441d8326a55539437ffd8305598702712c7c3fe
Dominic Salsbury <dominicsalsbury@proton.me>
2026-02-23 20:32:14 +0000

    added a list of Part types for storing the messages within the conversation. Initially we store starting user prompt as a part in that list.

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
+
+    messages = [types.Content(
+        role="user", parts=[types.Part(text=args.user_prompt)])]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -19,7 +23,7 @@ def main():
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=messages,
     )
     if not response.usage_metadata:
         raise RuntimeError(
