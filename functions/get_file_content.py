15bc36df67344953c1e1da7419c3ad16aa2a4c8a
Dominic Salsbury <dominicsalsbury@proton.me>
2026-02-24 12:18:53 +0000

    added get_file_content module to to read content from files within allowed directory. Reads content up-to 1000 chars before truncating to save on tokens when submitting to LLM models. Configurable by MAX_CHARS variables in config.py

---
+++ functions/get_file_content.py
@@ -0,0 +1,29 @@
+import os
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        file_path_abs = os.path.normpath(
+            os.path.join(working_dir_abs, file_path))
+
+        valid_target_path = os.path.commonpath(
+            [working_dir_abs, file_path_abs]) == working_dir_abs
+
+        if not valid_target_path:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(file_path_abs):
+            f'Error: File not found or is not a regular file: "{file_path}"'
+
+        with open(file_path_abs, "r") as f:
+            content = f.read(MAX_CHARS)
+            # Checking if file was larger than the limit. By reading again it checks the next character after the initial MAX_CHARS
+            if f.read(1):
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
+    except Exception as e:
+        return f'Error reading file: "{file_path}": {e}'
